6bb333ab64f0a74f4063e598aec8eda8e7caeb29
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-07-29 16:22:44 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+// REDUX CODE
+///////////////////////////////////
+
+const toggle = () => {
+  return {type: 'toggle'} 
+}
+ 
+const initialState = 'off';
+const lightSwitchReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'toggle':
+      return state === 'on' ? 'off' : 'on';
+    default:
+      return state; 
+  }
+} 
+ 
+const store = createStore(lightSwitchReducer);
+
+// REACT CODE
+///////////////////////////////////
+ 
+// Pass the store's current state as a prop to the LightSwitch component.
+const render = () => {
+  ReactDOM.render(
+    <LightSwitch 
+      state={store.getState()}
+    />,
+    document.getElementById('root')
+  )
+}
+ 
+render(); // Execute once to render with the initial state.
+store.subscribe(render); // Re-render in response to state changes.
+
+// Receive the store's state as a prop.
+function LightSwitch(props) {
+  const state = props.state; 
+
+  // Adjust the UI based on the store's current state.
+  const bgColor = state === 'on' ? 'white' : 'black';
+  const textColor = state === 'on' ? 'black' : 'white';  
+ 
+  // The click handler dispatches an action to the store.
+  const handleLightSwitchClick = () => {
+    store.dispatch(toggle());
+  }
+ 
+  return (  
+    <div style={{background : bgColor, color: textColor, padding: '7em 10em 10em 15em', border: 'solid black 3px',  
+    borderRadius: '5px'}}>
+      <button style={{fontSize: '3em', fontWeight: 'bold', border: 'solid #A9A9A9 5px', color: 'white', backgroundColor: '#800000', borderRadius: '5px', padding: '15px 15px 15px 15px'}}onClick={handleLightSwitchClick}>
+        {state}
+      </button>
+    </div>
+  )
+}
+ 
